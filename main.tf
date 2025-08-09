9d8b7c2f5c721a4213c4dbf43a087b430930c886
sebatapiaval <seba.tapia.val@gmail.com>
2025-08-09 19:07:31 -0400

    actualizacion de archivos tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "google_compute_instance" "default" {
+  name         = "vm-dev"
+  machine_type = "e2-micro"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+  description = "GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region"
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "GCP zone"
+  default     = "us-central1-a"
+}
